2c7f83b7ad8aca6018e2042ebc20a606afea4e05
Andrew Hollingworth <andrew.hollingworth@datadoghq.com>
2025-03-11 00:08:52 -0400

    tiny express server, cpu data!

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { nodePolyfills } from 'vite-plugin-node-polyfills'
 import tsconfigPaths from "vite-tsconfig-paths"
 
 
@@ -8,9 +7,6 @@ import tsconfigPaths from "vite-tsconfig-paths"
 export default defineConfig({
   plugins: [
     react(),
-    nodePolyfills({
-      protocolImports: true
-    }),
     tsconfigPaths(),
   ]
 })
